fefe28565360b7105aab0ebbbaf32fb58ccef986
11Midnight <47757447+11Midnight@users.noreply.github.com>
2019-11-06 18:04:21 +0200

    Update edituserbyadmin.php

--- edituserbyadmin.php
+++ edituserbyadmin.php
@@ -49,7 +49,7 @@ if($result)
 mysqli_close($link); 
 ?>
 <link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" href="style1.css">
 
 
 <!doctype html>
@@ -68,7 +68,6 @@ mysqli_close($link);
    <meta name="viewport"
          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
    <style>
        .container {
            width: 400px;
@@ -81,8 +80,12 @@ mysqli_close($link);
        <p class="link5"><img class="link6" src="public/images/<?=$curPhoto?>" width="160" height="160"></p> First Name: <input type="text" name="first_name" value="<?=$curFirstName?>">   
 	   <p class="link3"><input type="file" name="fileToUpload" id="fileToUpload" value="select img"></p>Last Name: <input type="text" name="last_name"value="<?=$curLastName?>">
 	   Email: <input type="text" name="email"value="<?=$curEmail?>"><br>
-	   Password: <input type="password" name="password"><br>
-	   Role: <input type="text" name="role"value="<?=$curIdRole?>"><br>
+	   Password: <input type="password" name="password" minlength="6"><br>
+	    <p><select  required  name="role">
+		<option >Выберите роль</option>
+		<option value="1">Admin</option>
+		<option value="2">User</option>
+		</select></p>
        <p class="link2"><input type="submit" class="btn" value ="Edit"></p>
    </form>
    
